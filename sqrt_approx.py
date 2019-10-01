81ffea8f5f0471038d64018a057fa5f7ccae844c
James Q <jamesleroyq@gmail.com>
2019-10-01 01:55:00 +0100

    created a python code for approximating the square root of a float

---
+++ sqrt_approx.py
@@ -0,0 +1,20 @@
+#adapted from the lecture of Dr. Ian McLoughlin
+
+def sqrt(x):
+    # Initial guess or start number using a float to tell python to use float instead of an int
+    z = 1.0
+
+    # tell python to calculate a better estimate for the
+    # square root of x until within set number of decimal places
+
+    while abs(z*z - x) >=0.0001
+    
+        z -=(z*z - x) / (2*z)
+
+    return z
+
+z = sqrt(63.0)
+
+print(z)
+
+print(z*z)
